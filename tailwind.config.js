bd80a1512d6954a68d3580408b55b2fa6c0737db
Petrik Siano <petriksiano24@gmail.com>
2026-03-28 21:07:11 +0700

    rebuilding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  './index.html',
-  './src/**/*.{js,ts,jsx,tsx,html}',
+    './public/index.html',
+    './src/**/*.{js,ts,jsx,tsx,html}',
   ],
   darkMode: 'class',
   theme: {
@@ -39,3 +40,4 @@ module.exports = {
   },
   plugins: [],
 };
+
